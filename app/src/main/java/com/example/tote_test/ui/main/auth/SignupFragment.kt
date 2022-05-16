5f338221fd97b46f2dc0ae6ce3b4ada41c4eeed8
YurijM <y-myagkov@mail.ru>
2022-05-16 16:35:41 +0300

    Небольшие изменения

--- app/src/main/java/com/example/tote_test/ui/main/auth/SignupFragment.kt
+++ app/src/main/java/com/example/tote_test/ui/main/auth/SignupFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.tote_test.R
 
-class RegistrationFragment : Fragment() {
+class SignupFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
